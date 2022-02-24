c16aa5b117ac500ff9bd2e7770bc69b21b24b3ee
kagwicharles <ckagwi8@gmail.com>
2022-02-24 12:03:10 +0300

    ui changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ export default function Home() {
                 <Grid container className='container align-items-center'>
                     <Grid item xs={6}>
                         <Stack spacing={2} justifyContent='center'>
-                            <Typography variant='h3' align='left'>
+                            <Typography className="font-face-roboto-black" variant='h3' align='left'>
                                 Accurate predictions for Malaria, Pneumonia and Covid19.
                             </Typography>
                             <Typography variant="p"
